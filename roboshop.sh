5ee51812fdf2e8feeaadda7b13bb7358593f75ce
JagadishNaidu1994 <babblusrinivas1994@gmail.com>
2023-12-20 13:19:25 +0530

    roboshop

--- roboshop.sh
+++ roboshop.sh
@@ -5,7 +5,7 @@ INSTANCE=("mongo" "redis" "mysql" "rabbitmq" "cart" "catalogue" "user" "shipping
 
 
 
-for i in "$($INSTANCE[@])"
+for i in "${$INSTANCE[@]}"
 
 do
     if [$i=="mongo"] || [$i=="mysql"] || [$i=="shipping"]
@@ -15,6 +15,6 @@ do
         INSTANCE_TYPE="t2.micro"
     fi
 
-        aws ec2 run-instances --image-id ami-03265a0778a880afb --count 1 --instance-type $INSTANCE_TYPE --security-group-ids sg-0c55d53f300d3cd20
+        aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type $INSTANCE_TYPE --security-group-ids sg-0c55d53f300d3cd20
 
 done
