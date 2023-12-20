16f3b5e78ff1b16779cd11115a8e2c8239ca7455
JagadishNaidu1994 <babblusrinivas1994@gmail.com>
2023-12-20 13:28:31 +0530

    roboshop

--- roboshop.sh
+++ roboshop.sh
@@ -15,6 +15,6 @@ do
         INSTANCE_TYPE="t2.micro"
     fi
 
-        aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type $INSTANCE_TYPE --security-group-ids sg-0c55d53f300d3cd20
+        aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type $INSTANCE_TYPE --security-group-ids sg-0c55d53f300d3cd20 --tags Key=Name,Value=$i
 
 done
